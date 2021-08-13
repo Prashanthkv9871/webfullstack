02a1e2e96412bfc5d39d2973af4bfe1fbd63d322
Prashanthkv9871 <kvprashanth987@gmail.com>
2021-08-13 11:59:26 +0530

    javascript Assignment

---
+++ javascript Assignment1/for5to15.js
@@ -0,0 +1,5 @@
+// 3.Write a program to print 5 to 15 numbers using For Loop
+
+for(let i=5;i<=15;i++){
+    console.log(i);
+}
---
+++ javascript Assignment1/while10to1.js
@@ -0,0 +1,8 @@
+//2. Write a program to print 10 to 1 number using a while loop
+
+let i=10;
+
+while(i>0){
+    console.log(i);
+    i--;
+}
