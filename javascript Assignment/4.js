0331e1d930309b174e8760d961e0224b34c2deb6
Prashanthkv9871 <kvprashanth987@gmail.com>
2021-08-13 09:58:10 +0530

    javascript assignment

---
+++ javascript Assignment/4.js
@@ -0,0 +1,11 @@
+// 4. Write a program to check a number is having 4 at unit's place or not?|
+let num=445;
+
+if(num%10==4)
+{
+    console.log(num + " is ending with digit 4");
+}
+
+else{
+    console.log(num + " is not ending with digit 4")
+}
