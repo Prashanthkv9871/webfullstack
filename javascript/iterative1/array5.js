291285a6017e406a2ba3763fa4fdd6396d701637
Prashanthkv9871 <kvprashanth987@gmail.com>
2021-08-17 23:27:36 +0530

    web

---
+++ javascript/iterative1/array5.js
@@ -0,0 +1,12 @@
+let a = [];
+console.log(a);
+let size = [38, 40, 42, 44];
+
+let marks = [65, 75, 85, 95, 96, 95];
+let arr = ['one', 2, false, true,[3,3,2]];
+
+console.log(size);
+console.log(marks);
+console.log(arr);
+
+console.log(arr.b[4]);
---
+++ javascript/iterative1/forof.js
@@ -0,0 +1,6 @@
+let final_Exams_marks = [35, 36, 37, 38, 39];
+
+for(marks of final_Exams_marks){
+    console.log(marks);
+}
+
