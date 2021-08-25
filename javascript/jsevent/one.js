9b2e09b48d60f5c3053cddc83eeac77ac81f2c43
Prashanthkv9871 <kvprashanth987@gmail.com>
2021-08-25 08:20:26 +0530

    events

---
+++ javascript/jsevent/one.js
@@ -0,0 +1,31 @@
+function mouseover_change(){
+    document.getElementById("mouseover").style.backgroundColor="green";
+    document.getElementById("mouseover").style.color="white";
+
+}
+
+function mouseout_change(){
+    document.getElementById("mouseout").style.backgroundColor="yellow";
+    document.getElementById("mouseout").style.color="white";
+}
+
+function click_change(){
+    document.getElementById("click").style.backgroundColor="pink";
+    document.getElementById("click").style.color="white";
+}
+
+function dbl_change(){
+    document.getElementById("double").style.backgroundColor="yellow";
+    document.getElementById("double").style.color="white";
+}
+
+function focus_change(){
+    document.getElementById("focus").style.backgroundColor="green";
+    document.getElementById("focus").style.color="white";
+}
+
+function change_upper(){
+    
+    document.getElementById("blur").style.color="black";
+}
+
