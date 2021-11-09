5ad8297ab0a580b5c9c54ec7b3da9fbea015f281
Prashanthkv9871 <kvprashanth987@gmail.com>
2021-11-09 12:58:59 +0530

    java

---
+++ JAVA/Control flow/Test.java
@@ -0,0 +1,11 @@
+class Test{
+    public static void main(String[] args){
+        int salary=40000;
+        if(salary>35000){
+            System.out.println("Salary is greater");
+        }
+        else{
+            System.out.println("Salary is Lesser");
+        }
+    }
+}
